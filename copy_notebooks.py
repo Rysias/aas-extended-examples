176688d603c101cd8bf22cd7da26c09d22d5a80f
Rysias <jhvithamar@gmail.com>
2022-10-13 11:38:55 +0100

    create notebooks

---
+++ copy_notebooks.py
@@ -0,0 +1,18 @@
+"""Create a scrapbook of question notebooks for personal editing"""
+from pathlib import Path
+import shutil
+
+
+def main() -> None:
+    # Find all -questions.ipynb files
+    notebooks = list(Path(".").rglob("*questions.ipynb"))
+    print(f"Found {len(notebooks)} notebooks")
+    # Create a copy in each directory with "JHR" appended
+    for notebook in notebooks:
+        new_name = notebook.with_name(notebook.stem + "-JHR" + notebook.suffix)
+        if not new_name.exists():
+            shutil.copy(notebook, new_name)
+
+
+if __name__ == "__main__":
+    main()
